59a2afa028c83f05924b5ac0f667c0d1e277d92a
Andrea Batugo <abatugo@Andreas-MBP.fios-router.home>
2020-08-05 15:03:15 -0400

    Updates

---
+++ precache-manifest.88ad83cee01545262af7c5d8f91eb12a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5252696ddc589e4e5e4a2a4b3856b2",
+    "url": "/priorities/index.html"
+  },
+  {
+    "revision": "2c4b0ab4563f0914c8d0",
+    "url": "/priorities/static/css/2.9df7aa81.chunk.css"
+  },
+  {
+    "revision": "0476a02c00def161c1c7",
+    "url": "/priorities/static/css/main.e179cfe9.chunk.css"
+  },
+  {
+    "revision": "2c4b0ab4563f0914c8d0",
+    "url": "/priorities/static/js/2.6faaffde.chunk.js"
+  },
+  {
+    "revision": "dfcf43b1a4f4d764af379e343944558c",
+    "url": "/priorities/static/js/2.6faaffde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0476a02c00def161c1c7",
+    "url": "/priorities/static/js/main.7341a593.chunk.js"
+  },
+  {
+    "revision": "c61488ffb4c44d807856",
+    "url": "/priorities/static/js/runtime-main.377a9202.js"
+  }
+]);
